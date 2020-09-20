660b58329d50ccd4a20415dd66b44edafe3cab6e
Joe Buhlig <joe@joebuhlig.com>
2020-09-20 14:31:50 -0500

    working web hooks!

--- app/controllers/discourse_stripe/webhook_controller.rb
+++ app/controllers/discourse_stripe/webhook_controller.rb
@@ -21,31 +21,64 @@ module DiscourseStripe
       rescue Stripe::SignatureVerificationError => e
         return 400
       end
-
-      if event["type"] == "customer.created"
-        print 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - CUSTOMER'
+      if event['type'] == 'customer.created'
         email = 'hello@joebuhlig.com'
         user = User.find_by_email(email)
-        if user
-          print user
-        else
-          print 'no user found'
+        unless user
           user = User.create!(
-              email: email,
-              username: UserNameSuggester.suggest(email),
-              name: User.suggest_name(email),
-              staged: true
-            )
+            email: email,
+            username: UserNameSuggester.suggest(email),
+            name: User.suggest_name(email),
+            staged: true
+          )
         end
-        print user
-        print event['data']['object']['id']
-        print event['data']['object']['email']
-      elsif event["type"] == "customer.subscription.created"
-        print 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - NEW'
+        user.custom_fields[:stripe_customer_id] = event['data']['object']['id']
+        user.save
+      elsif event['type'] == 'customer.subscription.created'
+        customer_id = event['data']['object']['customer']
+        user_cf = UserCustomField.find_by(value: customer_id)
+        if user_cf
+          user = User.find(user_cf.user.id)
+          if user
+            group_assignments = SiteSetting.discourse_stripe_group_assignment
+            group_assignments.split('|').each do |group|
+              product_id = group.split(':').last
+              event['data']['object']['items']['data'].each do |item|
+                if item['price']['product'] == product_id
+                  dc_group = Group.find_by(name: group.split(':').first)
+                  next unless dc_group
 
-      elsif event["type"] == "customer.subscrption.deleted"
-        print 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - DELETE'
-      end
+                  dc_group.add(user)
+                  dc_group.save
+                  return 200
+                end
+              end
+            end
+          end
+        end
+      elsif event['type'] == 'customer.subscription.deleted'
+        customer_id = event['data']['object']['customer']
+        user_cf = UserCustomField.find_by(value: customer_id)
+        if user_cf
+          user = User.find(user_cf.user.id)
+          if user
+            group_assignments = SiteSetting.discourse_stripe_group_assignment
+            group_assignments.split('|').each do |group|
+              product_id = group.split(':').last
+              event['data']['object']['items']['data'].each do |item|
+                if item['price']['product'] == product_id
+                  dc_group = Group.find_by(name: group.split(':').first)
+                  next unless dc_group
+
+                  dc_group.remove(user)
+                  dc_group.save
+                  return 200
+                end
+              end
+            end
+          end
+        end
+     end
 
       return 200
     end
